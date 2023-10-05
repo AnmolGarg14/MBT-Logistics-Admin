440b0af84a181126dd3a20910632a1de8bba580e
Anmol99535 <anmol.garg594@gmail.com>
2023-10-05 22:57:05 +0530

    Update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverActions: true,
+    appDir: true,
+    serverComponentsExternalPackages: ["mongoose"],
   },
   images: {
     domains: ["lh3.googleusercontent.com"],
